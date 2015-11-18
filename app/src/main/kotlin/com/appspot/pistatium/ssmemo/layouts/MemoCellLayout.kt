9cdc49936e95a9b2c8b74e973fa873b110b9bc36
Kimihiro N <kimihiro.nisimura@gmail.com>
2015-11-18 13:53:21 +0900

    WIP) Refactor to kotlin

--- app/src/main/kotlin/com/appspot/pistatium/ssmemo/layouts/MemoCellLayout.kt
+++ app/src/main/kotlin/com/appspot/pistatium/ssmemo/layouts/MemoCellLayout.kt
@@ -15,20 +15,21 @@ import com.appspot.pistatium.ssmemo.SSMemoApplication
 import com.appspot.pistatium.ssmemo.adapters.MemoCellInterface
 import com.appspot.pistatium.ssmemo.models.Memo
 import com.appspot.pistatium.ssmemo.models.Priority
+import kotlin.properties.Delegates
 
 
 /**
  * Created by kimihiro on 15/11/17.
  */
 class MemoCellLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
-    internal var tvMemo: TextView? = null
-    internal var vMemoTab: View = findViewById(R.id.memo_text) as TextView
-    internal var btnDelete: View? = null
-    internal var btnFav: View? = null
+    internal var tvMemo: TextView by Delegates.notNull()
+    internal var vMemoTab: View by Delegates.notNull()
+    internal var btnDelete: View by Delegates.notNull()
+    internal var btnFav: View by Delegates.notNull()
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-//        tvMemo = findViewById(R.id.memo_text) as TextView
+        tvMemo = findViewById(R.id.memo_text) as TextView
         vMemoTab = findViewById(R.id.memo_tab)
         btnDelete = findViewById(R.id.btn_memo_delete)
         btnFav = findViewById(R.id.btn_memo_fav)
@@ -36,7 +37,8 @@ class MemoCellLayout(context: Context, attrs: AttributeSet) : LinearLayout(conte
 
     fun bindView(memo: Memo, cellInterface: MemoCellInterface) {
         val app = context.applicationContext as SSMemoApplication
-        tvMemo!!.text = memo.memo
+
+        tvMemo.text = memo.memo
         app.setAppFont(tvMemo)
 
         if (memo.priority == Priority.HIGH.value) {
@@ -44,11 +46,13 @@ class MemoCellLayout(context: Context, attrs: AttributeSet) : LinearLayout(conte
         } else {
             vMemoTab.setBackgroundColor(ContextCompat.getColor(app, R.color.colorPrimary))
         }
-        btnFav!!.setOnClickListener { cellInterface.onClickFav(memo) }
-        btnDelete!!.setOnClickListener { cellInterface.onClickDelete(memo) }
+
+        btnFav.setOnClickListener { cellInterface.onClickFav(memo) }
+
+        btnDelete.setOnClickListener { cellInterface.onClickDelete(memo) }
+
         this.setOnClickListener {
-            val context = context
-            val i = EditActivity.createIntent(context, memo.id!!)
+            val i = EditActivity.createIntent(context, memo.id)
             context.startActivity(i)
         }
     }
