4db59e6ddff90823fe082f59c8cfa27ab10538bd
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-11-23 13:50:34 +0900

    Set big text style

--- app/src/main/kotlin/com/appspot/pistatium/ssmemo/models/MemoNotificationManager.kt
+++ app/src/main/kotlin/com/appspot/pistatium/ssmemo/models/MemoNotificationManager.kt
@@ -1,10 +1,11 @@
 package com.appspot.pistatium.ssmemo.models
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
 import android.support.v4.app.NotificationManagerCompat
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.NotificationCompat
+
+import com.appspot.pistatium.ssmemo.EditActivity
 import com.appspot.pistatium.ssmemo.R
 
 /**
@@ -15,15 +16,22 @@ class MemoNotificationManager(context:Context) {
     val context = context
 
     fun notify(memo: Memo) {
-        val builder = NotificationCompat.Builder(context)
-        val manager = NotificationManagerCompat.from(context);
+        val builder = Notification.Builder(context)
+        val manager = NotificationManagerCompat.from(context)
+        val memoId = getNotificationId(memo)
         builder.setSmallIcon(R.mipmap.icon512)
         builder.setContentTitle(context.getString(R.string.app_name))
         builder.setContentText(memo.memo)
-        manager.notify(getNotificationId(memo), builder.build())
+        val intent = EditActivity.createIntent(context, memo.id)
+        val contentIntent = PendingIntent.getActivity(context, memoId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(contentIntent)
+        builder.setAutoCancel(true)
+        builder.setStyle(Notification.BigTextStyle().bigText(memo.memo))
+        manager.notify(memoId, builder.build())
     }
 
     private fun getNotificationId(memo: Memo): Int {
-        return (memo.id % 100000) as Int
+        return (memo.id % 100000).toInt()
     }
+
 }
